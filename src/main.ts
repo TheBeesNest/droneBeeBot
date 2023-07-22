79f03f499e42a9697f5f88ff7b53141909b488c8
Myles Browne <myles@browne.link>
2023-07-22 18:02:22 +0100

    bot online

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
+import { Client, GatewayIntentBits, Events } from 'discord.js';
 import 'dotenv/config';
 
-console.log(process.env.test);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+console.log('starting up Bot!')
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.botApiToken);
