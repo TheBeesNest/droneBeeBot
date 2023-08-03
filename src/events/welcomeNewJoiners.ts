f3309bf8b74c7840adc0cac08cb4c47c77044212
Myles Browne <myles@browne.link>
2023-08-03 16:47:26 +0100

    roles are working now!

--- src/events/welcomeNewJoiners.ts
+++ src/events/welcomeNewJoiners.ts
@@ -5,12 +5,14 @@ export const name = Events.GuildMemberAdd;
 export const execute = (interaction: any) => {
 
 	const user = interaction.user as GuildMember
+	//const userObject = interaction.options.getMember(user) as GuildMember
+
 	const channel = interaction.client.channels.cache.get(process.env.welcome_channel) as TextChannel;
 	
 	var role = interaction.guild.roles.cache.find((role: Role) => role.name === 'test role') as Role;
-	
-	console.log(user)
-	
+
+	interaction.roles.add(role);
+		
 	channel.send(`Hi ${user}, welcome to the server`);
 
 } 
