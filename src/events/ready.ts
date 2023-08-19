94196d7119e12ce80d43687eedfc55f1270f018e
Myles Browne <myles@browne.link>
2023-08-19 15:50:43 +0100

    async code

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,6 @@ import { Events } from 'discord.js';
 
 export const name = Events.ClientReady;
 export const once = true;
-export const execute = (client: any) => {
+export const execute = async (client: any) => {
 	console.log(`Ready! Logged in as ${client.user.tag}`)};
 	
