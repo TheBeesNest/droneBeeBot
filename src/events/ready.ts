c210f1fa66d720567c07fab64971b1e9c704f170
Myles Browne <myles@browne.link>
2023-08-20 11:58:44 +0100

    organise

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,5 +3,6 @@ import { Events } from 'discord.js';
 export const name = Events.ClientReady;
 export const once = true;
 export const execute = async (client: any) => {
-	console.log(`Ready! Logged in as ${client.user.tag}`)};
+	console.log(`Ready! Logged in as ${client.user.tag}`)
+};
 	
