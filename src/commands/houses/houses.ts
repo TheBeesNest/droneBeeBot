13c8794de577fe87f34a56c8e82c5dcec6fd9035
Myles Browne <myles@browne.link>
2023-08-25 14:25:17 +0100

    working on house commands

--- src/commands/houses/houses.ts
+++ src/commands/houses/houses.ts
@@ -1,4 +1,7 @@
 import { ChatInputCommandInteraction, GuildMember, PermissionFlagsBits, Role, SlashCommandBuilder } from 'discord.js';
+import dbSource from '../../dbConnection';
+import { House } from '../../entity';
+import ErrorLogger from '../../classes/errorHandling';
 
 const houseList = [
 	{name: 'Honeysting', value: 'Honeysting'},
@@ -8,7 +11,7 @@ const houseList = [
 ]
 
 export const data = new SlashCommandBuilder()
-	.setName('house')
+	.setName('houses')
 	.setDescription('House Management')
 	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 	.addSubcommandGroup( group =>
@@ -33,6 +36,18 @@ export const data = new SlashCommandBuilder()
 							.setRequired(true)
 						)
 			)
+			.addSubcommand( subcommand =>
+				subcommand
+					.setName('remove')
+					.setDescription('remove a role assigned to a house (will not affect users)')
+					.addStringOption( option =>
+						option
+							.setName('house')
+							.setDescription('choose the house to have its role removed')
+							.setRequired(true)
+							.setChoices(...houseList)
+						)
+				)
 	)
 	.addSubcommandGroup( group =>
 		group
@@ -70,14 +85,50 @@ export const data = new SlashCommandBuilder()
 	)
 
 export const execute = async (interaction: ChatInputCommandInteraction) => {
+	const commandGroupSelected = interaction.options.getSubcommandGroup();
+	const commandSelected = interaction.options.getSubcommand();
+
 	const user = interaction.options.getMember('user') as GuildMember;
 	const houseValue = interaction.options.getString('house') as string;
+	const role = interaction.options.getRole('role') as Role;
 
 	await interaction.deferReply();
 
+	if (commandGroupSelected === 'roles') {
+		const houseRepo = dbSource.getRepository(House);
+
+		if (commandSelected === 'assign') {
+			const houseData = await houseRepo.findOne({where: {name: houseValue}});
+
+			if (houseData === null) {
+				interaction.editReply('i cant find that house... contact the master bee @choccobear');
+				return;
+			};
+			houseData.role = role.name;
+			try{
+				await houseRepo.save(houseData);
+				await interaction.editReply(`assigned role ${role} to house ${houseData.name}`);
+				return;
+			} catch(error) {
+				new ErrorLogger(error, 'houses#saveRoleToHouse', {houseData, houseValue, role});
+			}
 
 
-	await interaction.editReply(`welcome ${user} to house ${houseValue}`)
+		} else if (commandSelected === 'remove') {
+
+		}
+	} else if (commandGroupSelected === 'users') {
+		if (commandSelected === 'add') {
+
+		} else if (commandSelected === 'remove') {
+
+		}
+	}
+
+
+
+
+
 
 
 }
