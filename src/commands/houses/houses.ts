9596675b6669b40859ff2f7a6877ee58ab066f7a
Myles Browne <myles@browne.link>
2023-08-27 15:22:22 +0100

    houses working with management

--- src/commands/houses/houses.ts
+++ src/commands/houses/houses.ts
@@ -1,6 +1,6 @@
 import { ChatInputCommandInteraction, GuildMember, PermissionFlagsBits, Role, SlashCommandBuilder } from 'discord.js';
 import dbSource from '../../dbConnection';
-import { House } from '../../entity';
+import { House, User } from '../../entity';
 import ErrorLogger from '../../classes/errorHandling';
 
 const houseList = [
@@ -94,16 +94,17 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
 
 	await interaction.deferReply();
 
+	const houseRepo = dbSource.getRepository(House);
+	const houseData = await houseRepo.findOne({where: {name: houseValue}});
+	if (houseData === null) {
+		interaction.editReply('I cant find that house... contact the master bee @choccobear');
+		new ErrorLogger('house Missing', 'houses#saveRoleToHouse');
+		return;
+	};
+
 	if (commandGroupSelected === 'roles') {
-		const houseRepo = dbSource.getRepository(House);
 
 		if (commandSelected === 'assign') {
-			const houseData = await houseRepo.findOne({where: {name: houseValue}});
-
-			if (houseData === null) {
-				interaction.editReply('i cant find that house... contact the master bee @choccobear');
-				return;
-			};
 			houseData.role = role.name;
 			try{
 				await houseRepo.save(houseData);
@@ -112,23 +113,63 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
 			} catch(error) {
 				new ErrorLogger(error, 'houses#saveRoleToHouse', {houseData, houseValue, role});
 			}
-
-
 		} else if (commandSelected === 'remove') {
-
+			houseData.role = null;
+			try{
+				await houseRepo.save(houseData);
+				await interaction.editReply(`Cleared the role from house ${houseValue}`);
+				return;
+			} catch(error) {
+				new ErrorLogger(error, 'houses#removeRoleFromHouse', {houseData, houseValue, role});
+			}
 		}
 	} else if (commandGroupSelected === 'users') {
+		if (interaction.guild === null || houseData.role === null) { return };
+
+		const role = interaction.guild.roles.cache.find((role: Role) => role.name === houseData.role) as Role;
+		const userRepo = dbSource.getRepository(User);
+		const userData = await userRepo.findOne({where: { discordId: user.id}});
+
+		if (userData === null) {
+			interaction.editReply(`I've not seen this user before, get them talking and then we can give them a house.`);
+			new ErrorLogger('unknown user', 'houses#saveRoleToHouse');
+			return;
+		}
+
 		if (commandSelected === 'add') {
-
+			userData.houseId = houseData;
+			try {
+				await userRepo.save(userData);
+				await user.roles.add(role);
+				await interaction.editReply(`${user} has been added to ${houseValue}`);
+				return;
+			} catch (error) {
+				new ErrorLogger(error, 'houses#saveHouseToUser', {
+					houseData,
+					userData,
+					houseValue,
+					user,
+					commandGroupSelected,
+					commandSelected
+				});
+			}
 		} else if (commandSelected === 'remove') {
-
+			userData.houseId = null;
+			try {
+				await userRepo.save(userData);
+				await user.roles.remove(role);
+				await interaction.editReply(`${user} has been removed from ${houseData.name}`);
+				return;
+			} catch (error) {
+				new ErrorLogger(error, 'houses#removeHouseFromUser', {
+					houseData,
+					userData,
+					houseValue,
+					user,
+					commandGroupSelected,
+					commandSelected
+				});
+			}
 		}
 	}
-
-
-
-
-
-
-
 }
