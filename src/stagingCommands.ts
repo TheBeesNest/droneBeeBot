ae3788b463d93ab452381e9ab386f43a37f883f4
Myles Browne <myles@browne.link>
2023-10-08 21:58:27 +0100

    making her live!

--- src/stagingCommands.ts
+++ src/stagingCommands.ts
@@ -35,7 +35,7 @@ const rest = new REST().setToken(process.env.PROD_botApiToken as string);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data: any = await rest.put(
-			Routes.applicationCommands(process.env.PROD_clientId as string),
+			Routes.applicationGuildCommands(process.env.PROD_clientId as string, process.env.PROD_guildId as string),
 			{ body: commands },
 		);
 
