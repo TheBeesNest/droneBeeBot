e107a20c70fc6ec8b5b92f2fd19a117c6e566d9b
Myles Browne <myles@browne.link>
2023-11-29 21:45:04 +0000

    add staging

--- src/stagingConnection.ts
+++ src/stagingConnection.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 const productionUpdater = new DataSource({
 	type: 'mysql',
-	host: process.env.PROD_dbHost,
-	port: Number(process.env.PROD_dbPort),
-	database: process.env.PROD_database,
-	username: process.env.PROD_dbUsername,
-	password: process.env.PROD_dbPassword,
+	host: process.env.STAGING_dbHost,
+	port: Number(process.env.STAGING_dbPort),
+	database: process.env.STAGING_database,
+	username: process.env.STAGING_dbUsername,
+	password: process.env.STAGING_dbPassword,
 	entities: ['src/entity/*.ts'],
 	migrations: ['src/migration/*.ts'],
 
