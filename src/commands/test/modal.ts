cba16c8162a6397f945407a1628342b1817af772
Myles Browne <myles@browne.link>
2025-07-01 21:37:52 +0100

    modal test

--- src/commands/test/modal.ts
+++ src/commands/test/modal.ts
@@ -1,7 +1,12 @@
 import {
+	ActionRowBuilder,
 	ApplicationCommandType,
 	ContextMenuCommandBuilder,
 	ContextMenuCommandInteraction,
+	ModalActionRowComponentBuilder,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
 } from 'discord.js';
 
 export const data = new ContextMenuCommandBuilder()
@@ -11,5 +16,41 @@ export const data = new ContextMenuCommandBuilder()
 
 export const execute = async (interaction: ContextMenuCommandInteraction) => {
 	console.log(interaction);
+	const modal = new ModalBuilder()
+		.setCustomId('myModal')
+		.setTitle('My Modal');
+
+	// Add components to modal
+
+	// Create the text input components
+	const favoriteColorInput = new TextInputBuilder()
+		.setCustomId('favoriteColorInput')
+		// The label is the prompt the user sees for this input
+		.setLabel("What's your favorite color?")
+		// Short means only a single line of text
+		.setStyle(TextInputStyle.Short);
+
+	const hobbiesInput = new TextInputBuilder()
+		.setCustomId('hobbiesInput')
+		.setLabel("What's some of your favorite hobbies?")
+		// Paragraph means multiple lines of text.
+		.setStyle(TextInputStyle.Paragraph);
+
+	// An action row only holds one text input,
+	// so you need one action row per text input.
+	const firstActionRow =
+		new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+			favoriteColorInput,
+		);
+	const secondActionRow =
+		new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+			hobbiesInput,
+		);
+
+	// Add inputs to the modal
+	modal.addComponents(firstActionRow, secondActionRow);
+
+	// Show the modal to the user
+	await interaction.showModal(modal);
 	await interaction.reply({ ephemeral: true, content: 'pong' });
 };
